ea05d422b54c8e6d3efb178296bd23e734485b9d
ayamir <lgt986452565@gmail.com>
2021-11-19 10:41:48 +0800

    fix: remove rust-tools, enable vim-go.

--- lua/modules/lang/plugins.lua
+++ lua/modules/lang/plugins.lua
@@ -1,21 +1,13 @@
 local lang = {}
 local conf = require("modules.lang.config")
 
--- lang['fatih/vim-go'] = {
---     opt = true,
---     ft = 'go',
---     run = ':GoInstallBinaries',
---     config = conf.lang_go
--- }
-lang["rust-lang/rust.vim"] = {opt = true, ft = "rust"}
-lang["simrat39/rust-tools.nvim"] = {
+lang["fatih/vim-go"] = {
     opt = true,
-    ft = "rust",
-    config = conf.rust_tools,
-    requires = {
-        {"nvim-lua/plenary.nvim", opt = false}
-    }
+    ft = "go",
+    run = ":GoInstallBinaries",
+    config = conf.lang_go
 }
+lang["rust-lang/rust.vim"] = {opt = true, ft = "rust"}
 lang["kristijanhusak/orgmode.nvim"] = {
     opt = true,
     ft = "org",
