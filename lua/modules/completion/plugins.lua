30bf9e9542114c4acdcfa87ab9ce08eea9849969
ayamir <lgt986452565@gmail.com>
2021-11-19 10:41:20 +0800

    pref: more reasonable coc config file.

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -1,10 +1,12 @@
 local completion = {}
-local conf = require("modules.completion.config")
 
 completion["neoclide/coc.nvim"] = {
     opt = true,
     event = "BufRead",
-    branch = "release"
+    branch = "release",
+    config = function()
+        vim.api.nvim_exec([[source ~/.config/nvim/lua/modules/completion/coc_config.vim]], false)
+    end
 }
 completion["github/copilot.vim"] = {opt = true, cmd = "Copilot"}
 
